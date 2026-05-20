872f6d165f2c756a9db6938249423c11fdbd127f
Jeremy Lee <>
2026-05-20 19:26:40 +0800

    chore: remove test files

--- test_eta_debug.ts
+++
@@ -1,9 +0,0 @@
-import { config } from "dotenv";
-config();
-import { calculateEta } from "./src/services/truck.service";
-
-async function run() {
-  const eta = await calculateEta(24.8, 120.95);
-  console.log(eta);
-}
-run();
--- test_hccg.ts
+++
@@ -1,9 +0,0 @@
-import { fetchAllStops } from "./src/services/truck.service";
-async function run() {
-  const data = await fetchAllStops();
-  if (data && data.cleanPoint) {
-    const x = data.cleanPoint.filter(p => p.district === "3");
-    console.log(x.slice(0, 3).map(p => ({ n: p.pointName, t: p.trashDay, r: p.recycleDay })));
-  }
-}
-run();
